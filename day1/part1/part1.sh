1a3751d67350017d46de3a3f477946cc23ed0b1a
Thomas Lane <thomas_lane@icloud.com>
2018-12-01 11:34:30 -0800

    Update part1.sh

--- day1/part1/part1.sh
+++ day1/part1/part1.sh
@@ -5,7 +5,7 @@ sed 's/\([+-]\)/\1 /g' input.txt > stuff.txt
 
 #Runs math on it with the starting point of 0 like said by AoC then writes the answer to a file.
 #Note this process itself is 0.03 seconds if you dont write and allready have the file, making it the fastest slove atm.
-expr 0 $(stuff.txt) > output.txt
+expr 0 $(cat stuff.txt) > output.txt
 
 #Read the output
 cat output.txt
